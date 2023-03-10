7fc3f16ffb7d5e1537035cdae57f84d502e07f68
Selwyn-Lloyd McPherson <s.mcpherson@sound.ag>
2023-03-09 16:07:45 -0800

    Add /env endpoint

--- ecs/index.js
+++ ecs/index.js
@@ -9,7 +9,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/status', (req, res) => {
-  return res.send({ message: "Demo service is up"});
+  return res.send({ message: "Demo service is fancy"});
+})
+
+app.get('/env', (req, res) => {
+  return res.send({ message: process.env.ACCESS_KEY_ID})
 })
 
 app.listen(PORT, () => {
